27f2d2c83584489f479fece8f23ebf4419e3035d
ForidaSweety <foridayeasmin2000@gmail.com>
2024-02-25 21:56:57 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet-async";
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import PopularMenu from "../PopularMenu/PopularMenu";
+import Tesimonial from "../Testimonials/Tesimonial";
+
+
+const Home = () => {
+    return (
+        <div>
+             <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
+            <Banner></Banner>
+            <Category></Category>
+            <PopularMenu></PopularMenu>
+            <Featured></Featured>
+            <Tesimonial></Tesimonial>
+        </div>
+    );
+};
+
+export default Home;
